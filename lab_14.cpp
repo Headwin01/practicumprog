e59f4efc45ad4bb6e3f9a0d61ec2568c6bdcddd4
Headwin01 <75309190+Headwin01@users.noreply.github.com>
2020-12-06 23:48:34 +0200

    Add files via upload

---
+++ lab_14.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+    srand(time(0));
+    setlocale(0, "");
+    float value = 0;
+    vector<int> vec;
+    do
+    {
+        cout << "\nВведите число, для завершения введите 0\t";
+        cin >> value;
+        if (value != 0)
+             vec.push_back(value);
+
+    } while (value != 0);
+   
+    cout << "\nВсе введенные вами числа до введения нуля \n";
+    float sum = 0,
+        multiplay = 1;
+    for (size_t i = 0; i < vec.size(); i++)
+    {
+        sum += vec[i],
+        multiplay *= vec[i];
+        cout << vec[i] << " ";
+    }
+
+    double avrage_geom = (float)pow(multiplay, static_cast<double>(1. / (vec.size())));
+    cout << "\nСреднее арифметическое введеных вами чесел: " << sum/vec.size();
+    cout << "\nСреднее геометрическое введеных вами чесел: " << (double)avrage_geom;
+
+    return 0;
+}
