30f53e41b8f249135278c92b86fa57c90aa34bdb
Headwin01 <75309190+Headwin01@users.noreply.github.com>
2020-12-22 10:00:24 +0200

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,20 @@
+#include iostream
+#include math.h
+using namespace std;
+ 
+int main() {
+    int n, k;
+    float x, sum, i;
+    double y=-1;
+    cinn;
+    i=1; 
+    x=0;
+    sum=0;
+    for (k=1; k=n; k++) {
+        i=k;
+        x=x+pow((k+1), y);
+        sum+=ix;
+    }
+    coutsumma=sumendl;
+    return 0;
+}
